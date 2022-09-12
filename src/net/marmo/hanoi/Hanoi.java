7b5f8563a3f431f5b592d9df8596c690a76d0c3c
Marmo <marmo24391@gmail.com>
2022-09-12 11:39:09 +0200

    Update Hanoi.java

--- src/net/marmo/hanoi/Hanoi.java
+++ src/net/marmo/hanoi/Hanoi.java
@@ -5,7 +5,11 @@ import net.marmo.hanoi.game.Game;
 public class Hanoi {
 	
 	public static void main(String[] args) {
-		Game game = new Game(5);
+		int n = 5;
+		
+		System.out.println("n = " + n);
+		
+		Game game = new Game(n);
 		
 		game.solve();
 		
